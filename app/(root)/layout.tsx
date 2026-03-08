b880cd27549777aca32667e737ea0ae79e2695e1
Ali <alirezaeiy86@gamil.com>
2026-03-08 14:30:42 +0430

    eplement the full layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter,IBM_Plex_Serif } from "next/font/google";
 
 import { cn } from "@/lib/utils";
+import  Sidebar  from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
 
 
 
@@ -10,10 +13,26 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    const loggedIn={firstName:'Ali Akbar',lastName:'AKS'};
   return (
-   <main>
-    Sidebar
-    {children}
+   <main className="flex h-screen w-full font-inter">
+    <Sidebar user={loggedIn}/>
+    <div className="flex size-full flex-col ">
+        <div className="root-layout">
+            <Image
+            src="/icons/logo.svg" width={30}
+            height={30}
+            alt="logo"/>
+            <div>
+                <MobileNav user={loggedIn} />
+
+            </div>
+
+        </div>
+        {children}
+
+    </div>
+    
    </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,39 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSideba";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { access } from "fs";
 import React from "react";
 
 const Home= () => {
+
+    const  loggeIn={firstName:'Ali Akbar', lastName:'ALS',email:'alirezaeiy76@gmail.com'};
     return(
-        <div>
-            Home
-        </div>
+        <section className="home">
+
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                    type="greeting"
+                    title="Welcome"
+                    user={loggeIn?.firstName || 'Guest'}
+                    subtext="Access and manage Your account and Transactions efficiently."/>
+
+                    <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1250.35}
+                    />
+
+                </header>
+
+                RECENT TRANSACTIONS
+            </div>
+            <RightSidebar  
+            user={loggeIn}
+            transactions={[]}
+            banks={[{currentBalance:123.50},{currentBalance:500.50}]}
+            />
+        </section>
     )
 }
 export default Home;
